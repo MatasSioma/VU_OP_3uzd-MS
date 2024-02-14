73eb2736b194967d2b154b009d626045ae2bb23f
MatasSioma <matassemenas@gmail.com>
2024-02-14 23:59:26 +0200

    c_masyvas.cpp bazė

---
+++ c_masyvas.cpp
@@ -0,0 +1,17 @@
+#include <string>
+
+using namespace std;
+
+#define n 5 //nd uzduociu kiekis
+const int stud_sk = 5;
+
+struct studentas {
+    string vardas;
+    string pavarde;
+    int nd[n];
+    int egz;
+};
+
+int main() {
+    return 0;
+}
