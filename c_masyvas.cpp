96d92b4bb8e1231f95fb5f501021bcb753595076
MatasSioma <matassemenas@gmail.com>
2024-02-15 04:38:21 +0200

    Rakinis dumenu ivedimas

--- c_masyvas.cpp
+++ c_masyvas.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
-#include <string>
 #include <iomanip>
+#include <string>
+#include <limits>
 
 using namespace std;
 
 #define n 2 //nd uzduociu kiekis
-const int stud_sk = 3;
+const int stud_sk = 2;
 
 struct studentas {
-    string vardas = "vardas";
-    string pavarde = "pavarde";
+    string vardas;
+    string pavarde;
     int nd[n];
     int egz;
 };
@@ -22,7 +23,46 @@ int main() {
     cin >> option;
     switch(option) {
         case 1:
+            for (int i = 0; i < stud_sk; i++) {
+                cout << i+1 << "-ojo studento duomenys" << endl;
+
+                cout << "Vardas: ";
+                cin >> studentai[i].vardas;
+
+                cout << "Pavarde: ";
+                cin >> studentai[i].pavarde;
+
+                for(int gradeNum = 0; gradeNum < n + 1; gradeNum++) { // nd skaicius + 1 (egzamino ivertinimas)
+                    while(true) {
+                        int grade;
+                        if (gradeNum < n) {
+                            cout << gradeNum + 1 << " ND įvertinimas (0-10): ";
+                            cin >> grade;
+                            studentai[i].nd[gradeNum] = grade;
+                        } else {
+                            cout << "Egzamino įvertinimas: ";
+                            cin >> grade;
+                            studentai[i].egz = grade;
+                        }
+                        if (cin.fail()) { // Vartotojas iveda ne skaiciu
+                            cout << "Įveskite sveiką skaičių nuo 0 iki 10." << endl;
+                            cin.clear();
+                            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                            continue;
+                        }
+                        if (grade >= 0 && grade <= 10) {
+                            break;
+                        }
+                    }
+                }
+            }
             break;
+        case 2:
+            cout << "Funkcija dar nesukurta" << endl;
+            exit(1);
+        case 3:
+            cout << "Funkcija dar nesukurta" << endl;
+            exit(1);
         default:
             cout << "Blogai įvestas meniu pasirinkimas" << endl;
             exit(1);
@@ -33,6 +73,6 @@ int main() {
 
     for (int i = 0; i < stud_sk; i++) {
         // printf("%-14s%-14s\n", studentai[i].vardas.c_str(), studentai[i].pavarde.c_str());
-        cout << left << setw(14) << studentai[i].vardas << studentai[i].pavarde << endl;
+        cout << left << setw(14) << studentai[i].vardas << left << setw(14) <<studentai[i].pavarde << studentai[i].nd[0] << studentai[i].nd[1] << studentai[i].egz << endl;
     }
 }
