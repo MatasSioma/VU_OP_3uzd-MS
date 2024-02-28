2a2e37faf2f9eaeb7209d40685635669412998b2
MatasSioma <matassemenas@gmail.com>
2024-02-28 23:05:24 +0200

    Pagalbines funkcijos perkeltos i helpers.cpp faila

--- pazymiai.h
+++ pazymiai.h
@@ -7,12 +7,21 @@
 using namespace std;
 
 struct studentas {
-    std::string vardas = "vardas";
+    string vardas = "vardas";
     string pavarde = "pavarde";
     vector<int> nd = {0};
     int egz = 0;
     double vidurkis = 0;
     double mediana = 0;
 };
+
+string atnaujintiMasyvaUzklausa(string uzklausa);
+void atnaujintiMasyva(vector<studentas> &studentai, int stud_sk, int nd_sk);
+void atspauzdintiMasyvoInfo(vector<studentas> &studentai);
+
+bool palygintiPagalVarda(const studentas &a, const studentas &b);
+bool palygintiPagalPavarde(const studentas &a, const studentas &b);
+bool palygintiPagalVidurki(const studentas &a, const studentas &b);
+bool palygintiPagalMediana(const studentas &a, const studentas &b);
 
 #endif
