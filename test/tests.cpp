1ecac795999e027e8702aa1f975aa6c7bd35004d
MatasSioma <matassemenas@gmail.com>
2024-05-17 11:07:58 +0300

    v2.0.2

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,22 +3,63 @@
 
 int ndSk = 5, studSk = 30, inputOption = 3;
 
-int Add(int a, int b) {
-    return a + b;
-}
-TEST(AdditionTest, HandlesPositiveInput) {
-    EXPECT_EQ(Add(1, 2), 3);
-}
-
-TEST(StudentasKlase, Konstruktorius) {
-    Studentas a("Matas", "Semenas");
-    EXPECT_EQ(a.getVardas(), "Matas");
+TEST(StudentasKlase, ConstructorWithNames) {
+    Studentas stud;
+    stud.setVardas("Matas");
+    stud.setPavarde("Semenas");
+    EXPECT_EQ(stud.getVardas(), "Matas");
+    EXPECT_EQ(stud.getPavarde(), "Semenas");
 }
 
-TEST(StudentasKlase, kopija) {
-    Studentas stud1("Matas", "Semenas");
-    Studentas stud2 = stud1;
-
-    EXPECT_EQ(stud1.getVardas(), stud2.getVardas());
-    EXPECT_EQ(stud1.getPavarde(), stud2.getPavarde());
+//Konstruktoriaus testas
+TEST(StudentasKlase, DefaultConstructor) {
+    Studentas stud;
+    EXPECT_EQ(stud.getVardas(), "BeVardis");
+    EXPECT_EQ(stud.getPavarde(), "BePavardis");
+}
+//Move konstruktoriaus testas
+TEST(StudentasKlase, MoveConstructor) {
+    Studentas stud;
+    stud.setVardas("Matas");
+    stud.setPavarde("Semenas");
+    Studentas stud2(std::move(stud));
+    EXPECT_EQ(stud2.getVardas(), "Matas");
+    EXPECT_EQ(stud2.getPavarde(), "Semenas");
+    GTEST_EXPECT_FALSE(stud.getVardas() == stud2.getVardas());
+    EXPECT_TRUE(stud.getVardas().empty());
+    EXPECT_TRUE(stud.getPavarde().empty());
+}
+//Copy konstruktoriaus testas
+TEST(StudentasKlase, CopyConstructor) {
+    Studentas stud;
+    stud.setVardas("Matas");
+    stud.setPavarde("Semenas");
+    Studentas stud2(stud);
+    EXPECT_EQ(stud2.getVardas(), "Matas");
+    EXPECT_EQ(stud2.getPavarde(), "Semenas");
+    EXPECT_EQ(stud.getVardas(), stud2.getVardas());
+}
+//Assign operatoriaus testas
+TEST(StudentasKlase, Assignment) {
+    Studentas stud;
+    stud.setVardas("Matas");
+    stud.setPavarde("Semenas");
+    Studentas stud2;
+    stud2 = stud;
+    EXPECT_EQ(stud2.getVardas(), "Matas");
+    EXPECT_EQ(stud2.getPavarde(), "Semenas");
+    EXPECT_EQ(stud.getVardas(), stud2.getVardas());
+}
+//Perkelimo priskyrimo operatoriaus testas
+TEST(StudentasKlase, MoveAssignment) {
+    Studentas stud;
+    stud.setVardas("Matas");
+    stud.setPavarde("Semenas");
+    Studentas stud2;
+    stud2 = std::move(stud);
+    EXPECT_EQ(stud2.getVardas(), "Matas");
+    EXPECT_EQ(stud2.getPavarde(), "Semenas");
+    GTEST_EXPECT_FALSE(stud.getVardas() == stud2.getVardas());
+    EXPECT_TRUE(stud.getVardas().empty());
+    EXPECT_TRUE(stud.getPavarde().empty());
 }
