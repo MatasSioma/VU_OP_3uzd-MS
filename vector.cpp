b65e56360af900f13538d5e621eb372cb6fcb102
MatasSioma <matassemenas@gmail.com>
2024-05-23 00:34:32 +0300

    initializer_list, move operator

--- vector.cpp
+++ vector.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
-#include "vector.h"
 #include <cmath>
+#include <algorithm>
 
-int nextCapacity(const size_t &size) {
-    int pow = ceil(log((double)size) / log(2.0));
-    return pow;
+#include "vector.h"
+
+size_t nextCapacity(const size_t &size) {
+    size_t capacity = 2;
+    int power = ceil(log((double)size) / log(2.0));
+    for (int i = 0; i < power; i++) capacity *= 2;
+    return capacity;
 }
 
 template<typename T>
@@ -12,9 +16,20 @@ Vector<T>::Vector() : data_(nullptr), size_(0), capacity_(0) {};
 
 template<typename T>
 Vector<T>::Vector(size_t initial_size)
-    : size_(initial_size), capacity_(initial_size), data_(new T[initial_size]) {
-    // Initialize memory to default of T
-    for (size_t i = 0; i < size_; ++i) {
-        data_[i] = T();
-    }
+    : size_(initial_size), capacity_(nextCapacity(initial_size)), data_(new T[nextCapacity(initial_size)]) {
+    for (size_t i = 0; i < size_; i++) data_[i] = T();
+}
+
+template<typename T>
+Vector<T>::Vector(const Vector& other)
+    : size_(other.size_), capacity_(other.capacity_), data_(new T[other.capacity_]) {
+    std::copy(other.data_, other.data_ + other.size_, data_);
+}
+
+template<typename T>
+Vector<T>::Vector(Vector&& other) noexcept
+    : data_(other.data_), size_(other.size_), capacity_(other.capacity_) {
+    other.data_ = nullptr;
+    other.size_ = 0;
+    other.capacity_ = 0;
 }
